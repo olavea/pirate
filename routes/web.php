f1885e65b6fe1744a474c2c23936baa83f18c1da
olavea <ola@olavea.com>
2025-02-22 12:40:14 +0100

    Validate, never trust the user day 17 #48

--- routes/web.php
+++ routes/web.php
@@ -59,6 +59,10 @@ Route::get('/jobs/create', function () {
 
 Route::post('/jobs', function () {
     // skip validation dd(request()->all());
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
     Job::create([
         'title' => request('title'),
         'salary' => request('salary'),
